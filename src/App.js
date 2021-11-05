e16eeb58c197aca688d87134b99622b9e7e0b88c
Eddie Jaoude <eddie@jaoudestudios.com>
2021-11-05 17:37:51 +0000

    feat: prime react (#1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+import { Card } from "primereact/card";
 
 function App() {
   return (
     <div>
       <header>
         <h1>Community event calendar</h1>
+        <Card title="Title" subTitle="SubTitle">
+          Content
+        </Card>
       </header>
     </div>
   );
