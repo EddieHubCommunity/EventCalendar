89a6e57c32e96a572be61bac45200620503519fb
Eddie Jaoude <eddie@jaoudestudios.com>
2021-11-05 22:25:56 +0000

    feat: refactored app

--- src/App.js
+++ src/App.js
@@ -1,47 +1,124 @@
 import "./App.css";
+
+import React, { useState, useEffect } from "react";
+
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import { DataView } from "primereact/dataview";
+import { Chip } from "primereact/chip";
+import { Menubar } from "primereact/menubar";
 
-import { Card } from "primereact/card";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 function App() {
-  const events = [
+  const colors = {
+    twitter: {
+      background: "blue",
+      color: "white",
+    },
+    youtube: {
+      background: "red",
+      color: "white",
+    },
+  };
+
+  const [display, setDisplay] = useState("calendar");
+  const [events, setEvents] = useState([]);
+  const navBar = [
+    // {
+    //   label: "Grid",
+    //   icon: "pi pi-fw pi-th-large",
+    //   command: () => {
+    //     setDisplay("grid");
+    //   },
+    // },
     {
-      type: "TwitterSpace", // Live Stream
-      location: {
-        type: "virtual",
-        address: "London",
+      label: "Calendar",
+      icon: "pi pi-fw pi-calendar",
+      command: () => {
+        setDisplay("calendar");
       },
-      url: "http://eddiehub.org",
-      author: "Eddie Jaoude",
-      host: "Nick",
-      title: "All Day Event very long title",
-      date: {
-        start: "2019-01-01",
-        end: "2019-01-02",
+    },
+    {
+      label: "List",
+      icon: "pi pi-fw pi-list",
+      command: () => {
+        setDisplay("list");
       },
-      time: {
-        start: "0800",
-        end: "1000",
-        timezone: "UTC",
-      },
-      description:
-        "Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description Event description",
     },
   ];
+
+  useEffect(() => {
+    fetch("/events.json")
+      .then((response) => response.json())
+      .then((data) =>
+        data.sort((a, b) => new Date(a.start) - new Date(b.start))
+      )
+      .then((data) => setEvents(data));
+  }, []);
+
+  const renderListItem = (data) => {
+    return (
+      <>
+        <div
+          className="p-dataview-header"
+          onClick={() => (window.location = data.url)}
+        >
+          <Chip
+            label={data.title}
+            icon={`pi pi-${data.type}`}
+            style={{
+              background: colors[data.type].background,
+              color: colors[data.type].color,
+            }}
+          />
+        </div>
+        <div>
+          <p>{data.description}</p>
+        </div>
+        <div className="p-dataview-footer">Added by {data.author.name}</div>
+      </>
+    );
+  };
+
+  const renderGridItem = (data) => {
+    return <div className="p-dataview-grid">Not implemented yet</div>;
+  };
 
   return (
     <div>
       <header>
-        <h1>Community event calendar</h1>
+        <Menubar
+          model={navBar}
+          end={
+            <a href="https://github.com/EddieHubCommunity/EventCalendar">
+              <i className="pi pi-github" style={{ fontSize: "2em" }}></i>
+            </a>
+          }
+        />
       </header>
 
-      {events.map((event) => (
-        <Card title={event.title} subTitle={event.date.start}>
-          {event.description}
-        </Card>
-      ))}
+      <div>
+        {display !== "calendar" && (
+          <DataView
+            value={events.filter((item) => new Date(item.start) > new Date())}
+            layout={display}
+            itemTemplate={display === "list" ? renderListItem : renderGridItem}
+            paginator
+            rows={10}
+          />
+        )}
+
+        {display === "calendar" && (
+          <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            events={events}
+          />
+        )}
+      </div>
     </div>
   );
 }
